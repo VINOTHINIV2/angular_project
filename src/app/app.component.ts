79355518e09b36ae184e29f65043a6ed954f29c5
VINOTHINIV2 <109685848+VINOTHINIV2@users.noreply.github.com>
2022-07-23 15:28:26 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,38 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'ANGULAR ' + VERSION.major;
-  public headers = ['S no', 'Name', 'Class','Team','Score'];
+  public headers = ['Sno', 'Name', 'Class','Team','Score'];
   public rows = [
     {
-      S no :'1',
+      Sno :'1',
       Name :'Vicky',
       Class :'B',
       Team :'Red',
       Score :'85',
     },
     {
-      S no :'2',
+      Sno :'2',
       Name :'Princy',
       Class :'A',
       Team :'Blue',
       Score :'80',
     },
     {
-      S no :'3',
+      Sno :'3',
       Name :'Aravind',
       Class :'C',
       Team :'Green',
       Score :'90',
     },
     {
-      S no :'4',
+      Sno :'4',
       Name :'Karthi',
       Class :'B',
       Team :'Yellow',
       Score :'75',
     },
     {
-      S no :'5',
+      Sno :'5',
       Name :'Priya',
       Class :'A',
       Team :'Red',
